249a5d721384dad5bdab1357ae13dfa7006f2273
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-11-04 23:55:04 +0100

    input players

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 @Component({
   selector: 'app-game',
@@ -11,7 +13,7 @@ export class GameComponent implements OnInit {
   currentCard: string = '';
   game: Game = new Game();
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
 
   }
 
@@ -36,6 +38,14 @@ export class GameComponent implements OnInit {
         }, 1000);
       } 
     }
+  }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      this.game.players.push(name);
+    });
   }
 }
+
