c9ec16733de4d65540c50b87e8c512440c5c8485
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-11-05 02:10:47 +0100

    some small bugs fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { GameInfoComponent } from './game-info/game-info.component';
--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-player',
   templateUrl: './dialog-add-player.component.html',
-  styleUrls: ['./dialog-add-player.component.scss']
+  styleUrls: ['./dialog-add-player.component.scss'],
 })
 export class DialogAddPlayerComponent {
   name: string = '';
 
-  onNoClick() {
-    
+  constructor(
+    public dialogRef: MatDialogRef<DialogAddPlayerComponent>
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
